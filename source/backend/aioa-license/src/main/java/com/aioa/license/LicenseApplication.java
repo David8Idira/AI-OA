83226a58279f1c99cbb4293cc98e2c97d65abebe
A1 <a1@aioa.com>
2026-04-30 08:44:54 +0800

    fix(aioa-license): 移除EnableDiscoveryClient注解（无对应spring-cloud依赖）

--- source/backend/aioa-license/src/main/java/com/aioa/license/LicenseApplication.java
+++ source/backend/aioa-license/src/main/java/com/aioa/license/LicenseApplication.java
@@ -2,13 +2,10 @@ package com.aioa.license;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
 /**
  * 证照管理模块启动类
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 public class LicenseApplication {
     
     public static void main(String[] args) {
